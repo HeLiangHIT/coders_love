5525de2a16d2ff0ef07ab820119002c0542004c2
heliang <1042248300@qq.com>
2018-05-20 14:57:11 +0800

    first commit

---
+++ picture_wall.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2018-05-20 10:26:10
+# @Author  : He Liang (heianghit@foxmail.com)
+# @Link    : https://github.com/HeLiangHIT
+# @ref     : http://effbot.org/imagingbook/pil-index.htm
+# usage: python picture_wall.py --help
+
+
+from PIL import Image, ImageDraw, ImageFont
+import os, sys
+from clize import Parameter, run
+
+
+def list_file(filedir, sufix=None):
+    # 根据输入的后缀来列举文件夹中的文件，支持多个后缀
+    # sufix: str 类型的后缀，或者 list 类型的后缀列表
+    file_list = os.listdir(filedir)
+    file_list = [filedir + os.path.sep + f for f in file_list] # add path for files
+    if sufix is not None:
+        if isinstance(sufix, str): 
+            ret_list = [f for f in file_list if f.endswith(sufix)]
+        elif isinstance(sufix, list):
+            ret_list = [f for f in file_list if any([f.endswith(t) for t in sufix])]
+        else:
+            print(f"undefined input of sufix:{type(sufix)}")
+            ret_list = None
+        return ret_list
+    else:
+        return file_list
+# print(list_file("./img", ".jpeg"))
+# print(list_file("./", [".py","g"]))
+
+
+def picture_wall_rectangle(wall_width, wall_length, edge_len, pic_dir="./img"):
+    # 生成长方形的照片墙，输入：长和宽对应的照片数量，各个照片的边长，照片所在文件夹
+    new_img = Image.new('RGBA', (edge_len * wall_width, edge_len * wall_length))
+    file_list = list_file(pic_dir, ['.jpeg', '.jpg', '.gif', '.png', '.bmp'])
+    for x in range(0, wall_width):
+        for y in range(0, wall_length):
+            file_name = file_list[(x * wall_width + y) % len(file_list)]
+            try:
+                img = Image.open(file_name)
+                img = img.resize((edge_len, edge_len), Image.ANTIALIAS)
+                new_img.paste(img, (x * edge_len, y * edge_len))
+            except Exception as e:
+                print(f"open file {file_name} failed!")
+    return new_img
+# img_rec = picture_wall_rectangle(20, 10, 50, "./img")
+# img_rec.show()
+# print(img_rec.size)
+# img_rec.save('./out/img_rec.png')
+
+def gen_text_img(text, font_size=20, font_path=None):
+    # 从文字生成图像，输入：文字内容，文字字体大小，字体路径
+    text_img = Image.new('RGBA', ((len(text) - 1)* font_size, font_size))
+    font = ImageFont.truetype(font_path, font_size) if font_path is not None else None
+    draw = ImageDraw.Draw(text_img)
+    # 第一个tuple表示未知(left,up)，之后是文字，然后颜色，最后设置字体 
+    draw.text((font_size, 0), text, fill=(0,0,0), font=font) 
+    return text_img
+
+def picture_wall_mask(text_img, edge_len, pic_dir="./img"):
+    # 根据文字图像生成对应的照片墙，输入：文字图像，各个照片边长，照片所在路径
+    new_img = Image.new('RGBA', (text_img.size[0] * edge_len, text_img.size[1] * edge_len))
+    file_list = list_file(pic_dir, ['.jpeg', '.jpg', '.gif', '.png', '.bmp'])
+    img_index = 0
+    for x in range(0, text_img.size[0]):
+        for y in range(0, text_img.size[1]):
+            pixel = text_img.getpixel((x, y))
+            file_name = file_list[img_index % len(file_list)]
+            try:
+                img = Image.open(file_name).convert('RGBA')
+                img = img.resize((edge_len, edge_len), Image.ANTIALIAS)
+                _, _, _, alpha = img.split()
+                alpha = alpha.point(lambda i: pixel[-1])
+                img.putalpha(alpha)
+                new_img.paste(img, (x * edge_len, y * edge_len))
+                img_index += 1
+            except Exception as e:
+                print(f"open file {file_name} failed!")
+    return new_img
+# text_img = gen_text_img("I LOVE U", 20, './demo.ttf')
+# text_img.show()
+# text_img.save('./out/text_img.png')
+
+# img_ascii = picture_wall_mask(text_img, 50, "./img/")
+# img_ascii.show()
+# img_ascii.save('./out/img_ascii.png')
+
+
+# ref: http://clize.readthedocs.io/en/stable/basics.html#collecting-all-positional-arguments
+def main(*text, font_size:'s'=20, edge_len:'e'=50, wall_width:'w'=20, 
+        wall_length:'l'=10, pic_dir:'d'="./img", out_dir:'o'="./out",
+        font_path: 'p'='./demo.ttf'):
+    '''生成照片墙
+    
+    :param text: Text of picture wall, if not defined this will generage a rectangle picture wall
+    :param font_size: font size of a clear value
+    :param edge_len: sub picture's egde length
+    :param wall_width: picture number of rectangle width
+    :param wall_length: picture number of rectangle length
+    :param pic_dir: picture's path
+    :param out_dir: output dir
+    :param font_path: font path
+    '''
+    if len(text) >= 1:
+        text_ = ' '.join(text)
+        print(f"generate text wall for '{text_}' with picture path:{pic_dir}")
+        text_img = gen_text_img(text_, font_size, font_path)
+        img_ascii = picture_wall_mask(text_img, edge_len, pic_dir)
+        img_ascii.show()
+        img_ascii.save('./out/img_ascii.png')
+    else:
+        print(f"generate rectangle wall with picture path:{pic_dir}")
+        img_rec = picture_wall_rectangle(wall_width, wall_length, edge_len, pic_dir)
+        img_rec.show()
+        img_rec.save('./out/img_rec.png')
+
+if __name__ == '__main__':
+    run(main)
+
+
+
+
+
---
+++ readme.md
@@ -0,0 +1,42 @@
+[TOC]
+
+# 程序员的520怎么表白
+
+## 照片墙
+python picture_wall.py --help
+```
+Usage: ./picture_wall.py [OPTIONS] [text...]
+
+生成照片墙
+
+Arguments:
+  text...                 Text of picture wall, if not defined this will generage a rectangle picture wall
+
+Options:
+  -s, --font-size=INT     font size of a clear value (default: 20)
+  -e, --edge-len=INT      sub picture's egde length (default: 50)
+  -w, --wall-width=INT    picture number of rectangle width (default: 20)
+  -l, --wall-length=INT   picture number of rectangle length (default: 10)
+  -d, --pic-dir=STR       picture's path (default: ./img)
+  -o, --out-dir=STR       output dir (default: ./out)
+  -p, --font-path=STR     font path (default: ./demo.ttf)
+
+Other actions:
+  -h, --help              Show the help
+```
+for example:
+![./out/text_img.png](./out/text_img.png)
+
+### TODO
++ 字体大小不准确，待修正
+
+## 爱心情书
+
+
+
+
+
+
+
+
+
